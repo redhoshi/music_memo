9c8dddf27fb942cc605ef70270e71d3558ed0b7c
redhoshi <s1811396@s.tsukuba.ac.jp>
2021-08-23 11:17:49 +0900

    password管理

--- lib/Login/login.dart
+++ lib/Login/login.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_neumorphic/flutter_neumorphic.dart';
 import 'package:music_memo/first.dart';
@@ -14,7 +15,52 @@ class _LoginPageState extends State<LoginPage> {
   var _passwordController = TextEditingController();
   bool user_id = false;
   int press_id = 0;
+  final docList = [];
+  final password = [];
 
+//firestore
+  Future<void> User() async {
+    await FirebaseFirestore.instance
+        .collection('userid')
+        .get()
+        .then((QuerySnapshot querySnapshot) => {
+              querySnapshot.docs.forEach((doc) async {
+                docList.add(doc.id);
+              }),
+            });
+    print(docList);
+    for (int i = 0; i < docList.length; i++) {
+      final snepshot = await FirebaseFirestore.instance
+          .collection('userid')
+          .doc(docList[i])
+          .get();
+      password.add('${snepshot['password']}');
+    }
+    print('password:$password');
+  }
+
+  //Firebase上のパスワードとの一致
+  Future<void> CheckPass(_user, _pass) async {
+    for (int i = 0; i < docList.length; i++) {
+      if (_user == docList[i]) {
+        print('1');
+        if (_pass == password[i]) {
+          user_id = true;
+          print('2');
+          break;
+        } else {
+          user_id = false;
+          print("3");
+        }
+      } else {
+        user_id = false;
+      }
+    }
+
+    setState(() {});
+  }
+
+//正規表現
   Future<void> CheckPoint(_user) async {
     final result = new RegExp(r'^s[0-9]').hasMatch(_user);
     if (result != true) {
@@ -26,10 +72,12 @@ class _LoginPageState extends State<LoginPage> {
     }
   }
 
-  Future<void> TextApeal(press_id) async {
-    press_id > 0
-        ? Text('正しい値を入力せよ', textAlign: TextAlign.right)
-        : Text('$press_id');
+  //画面が作られたときに一度だけ呼ばれる
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    User();
   }
 
   @override
@@ -102,7 +150,11 @@ class _LoginPageState extends State<LoginPage> {
                 textAlign: TextAlign.center,
               ),
               onPressed: () async {
-                CheckPoint(_userController.text);
+                //CheckPoint(_userController.text);
+                CheckPass(_userController.text, _passwordController.text);
+                print(user_id);
+                print(
+                    'user:${_userController.text}pass:${_passwordController.text}');
                 print(user_id);
                 user_id != true
                     ? setState(() {
